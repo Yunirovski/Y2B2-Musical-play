9b4c0f5e2a9f389ba77a2dba808e70bc8e990f89
MeatChips <84376178+MeatChips@users.noreply.github.com>
2025-12-16 23:24:09 +0100

    Creatures update
    Its scuffed but it works

--- Y2B2 project/Assets/Script/Managers/CreatureManager.cs
+++ Y2B2 project/Assets/Script/Managers/CreatureManager.cs
@@ -2,77 +2,132 @@ using System;
 using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 
 public class CreatureManager : MonoBehaviour
 {
+    [SerializeField] private bool stateSwitch;
+
     [Header("Mic Settings")]
     [SerializeField] private MicrophoneListener listener;
     [SerializeField] private float loudnessMultiplier = 100f;
 
     [Header("Wake Up Settings")]
-    [SerializeField] private float wakeUpThreshold = 0.1f;
-    [SerializeField] private float requiredTimeAboveThreshold = 3f;
-    [SerializeField] private float requiredTimeBelowThreshold = 3f;
-    [SerializeField] private float timeAboveThreshold = 0f;
-    private float timeBelowThreshold = 0f;
+    [SerializeField] private float wu_wakeUpThreshold = 0.1f;
+    [SerializeField] private float wu_requiredTimeAboveThreshold = 3f;
+    [SerializeField] private float wu_requiredTimeBelowThreshold = 3f;
+    [SerializeField] private float wu_timeBelowThreshold = 0f;
+    [SerializeField] private float wu_timeAboveThreshold = 0f;
+    [SerializeField] private bool wu_isAwake = false;
+
+    [Header("Fall Asleep Settings")]
+    [SerializeField] private float fa_fallAsleepThreshold = 0.1f;
+    [SerializeField] private float fa_requiredTimeAboveThreshold = 3f;
+    [SerializeField] private float fa_requiredTimeBelowThreshold = 3f;
+    [SerializeField] private float fa_timeBelowThreshold = 0f;
+    [SerializeField] private float fa_timeAboveThreshold = 0f;
+    [SerializeField] private bool fa_isAsleep = false;
 
     [Header("Creature Sprite")]
     [SerializeField] private SpriteRenderer creatureSR;
     [SerializeField] private Sprite WakeUpSprite;
     [SerializeField] private Sprite AsleepSprite;
 
-    private bool isAwake = false;
+    public void switchBool()
+    {
+        stateSwitch = !stateSwitch;
+        wu_timeBelowThreshold = 0;
+        wu_timeAboveThreshold = 0;
+        wu_isAwake = false;
+
+        fa_timeAboveThreshold = 0;
+        fa_timeBelowThreshold = 0;
+        fa_isAsleep = false;
+    }
 
     void Update()
     {
-        WakeUp();
-    }
-
-    #region Wake Up
-    private void WakeUp()
-    {
-        if (isAwake)
-            return;
-
         float loudness = listener.GetLoudnessFromMic() * loudnessMultiplier;
 
-        if (loudness > wakeUpThreshold)
+        if (stateSwitch)
         {
-            timeAboveThreshold += Time.deltaTime;
+            CheckToWake(loudness);
+        }
+        else
+        {
+            CheckToSleep(loudness);
+        }
+    }
 
-            if (timeAboveThreshold >= requiredTimeAboveThreshold)
+    void CheckToWake(float currentLoudness)
+    {
+        if (wu_isAwake)
+            return;
+
+        creatureSR.sprite = AsleepSprite;
+
+        if (currentLoudness > wu_wakeUpThreshold)
+        {
+            wu_timeAboveThreshold += Time.deltaTime;
+
+            if(wu_timeAboveThreshold >= wu_requiredTimeAboveThreshold)
             {
                 WakeUpCreature();
             }
         }
         else
         {
-            timeBelowThreshold += Time.deltaTime;
+            wu_timeBelowThreshold += Time.deltaTime;
 
-            if (timeBelowThreshold >= requiredTimeBelowThreshold)
+            if(wu_timeBelowThreshold >= wu_requiredTimeBelowThreshold)
+            {
+                wu_timeBelowThreshold = 0;
+                wu_timeAboveThreshold = 0;
+                ItsTooQuiet();
+            }
+        }
+    }
+
+    void CheckToSleep(float currentLoudness)
+    {
+        if (fa_isAsleep) 
+            return;
+
+        creatureSR.sprite = WakeUpSprite;
+
+        if (currentLoudness <= fa_fallAsleepThreshold)
+        {
+            fa_timeBelowThreshold += Time.deltaTime;
+            fa_timeAboveThreshold = 0; 
+
+            if (fa_timeBelowThreshold >= fa_requiredTimeBelowThreshold)
             {
                 ItsTooQuiet();
+                fa_timeBelowThreshold = 0;
+            }
+        }
+        else
+        {
+            fa_timeAboveThreshold += Time.deltaTime;
+
+            if (fa_timeAboveThreshold >= fa_requiredTimeAboveThreshold)
+            {
+                fa_timeAboveThreshold = 0;
+                fa_timeBelowThreshold = 0;
+                WakeUpCreature();
             }
         }
     }
 
     private void WakeUpCreature()
     {
-        isAwake = true;
-
         creatureSR.sprite = WakeUpSprite;
-
-        Debug.Log("CREATURE IS AWAKE!!!");
+        wu_isAwake = true;
     }
 
     private void ItsTooQuiet()
     {
-        timeAboveThreshold = 0;
-        timeBelowThreshold = 0;
-
         creatureSR.sprite = AsleepSprite;
-
-        Debug.Log("ITS TOO QUIET!!!");
+        fa_isAsleep = true;
     }
-    #endregion
 }
